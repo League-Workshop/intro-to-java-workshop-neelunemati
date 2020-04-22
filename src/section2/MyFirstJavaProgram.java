f4bcf0cd82c4fe0077c071ba69ecf74abc8d65f3
root <root@6554b30b34b2>
2020-04-22 02:53:24 +0000

    my first java

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,17 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot rob = new Robot ();
 	
-		
+	rob.setSpeed(150);
+	rob.penDown();
+
+for(int i=0; i<4; i++){
+	rob.move(100);
+	rob.turn(90);
+}
+rob.miniaturize();
+rob.sparkle();
 		
 	}
 }
